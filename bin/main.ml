a62ba292916f8cdb6af520254657b424e9d09c40
Brendon Bown <brendon.bown@gmail.com>
2025-02-26 16:51:23 -0700

    Add tego parser that outputs s-expressions

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open! Core
 
-let read_standard_input () : string = In_channel.input_all In_channel.stdin
+(* let read_standard_input () : string = In_channel.input_all In_channel.stdin *)
 
 let parse_expr (input : string) : unit =
   let result = Expr_parser.Parser.parse input in
@@ -12,11 +12,29 @@ let parse_expr (input : string) : unit =
   | _ ->
       failwith "unreachable, parser is not ambiguous"
 
+let parse_tego (input : string) : unit =
+  let result = Tego_parser.Parser.parse_program input in
+  match result with
+  | [] ->
+      print_endline "invalid tego"
+  | [p] ->
+      let p = Tego_parser.Ast.Prog.sexp_of_t p |> Sexp.to_string_hum in
+      printf "%s\n" p
+  | result ->
+      printf "== AMBIGUOUS RESULTS : %d ======================\n"
+        (List.length result) ;
+      List.iteri result ~f:(fun i p ->
+          let p = Tego_parser.Ast.Prog.sexp_of_t p |> Sexp.to_string_hum in
+          printf "\n== RESULT %d ======================\n" i ;
+          printf "%s\n" p )
+
 let parse (parser : string) (input : string option) : unit =
-  let input = match input with Some s -> s | None -> read_standard_input () in
+  let input = match input with Some s -> s | None -> "" in
   match parser with
   | "expr" ->
       parse_expr input
+  | "tego" ->
+      parse_tego input
   | _ ->
       failwith "unknown parser"
 
@@ -33,7 +51,7 @@ let () =
   let parse_t = Term.(const parse $ parser_t $ input_t) in
   let cmd =
     let doc = "parse a string" in
-    let info = Cmd.info "parse" ~version:"%%VERSION%%" ~doc in
+    let info = Cmd.info "parse" ~doc in
     Cmd.v info parse_t
   in
   exit (Cmd.eval cmd)
